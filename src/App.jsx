0ab52d89de8af221766250e0a2d58510ffecdde8
Lucas <lucascostamonteiro@yahoo.com>
2025-05-23 19:12:46 -0700

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <h1>Word Search</h1>
+        <h2>Find the translation of words in the grid</h2>
+      </div>
+    </>
+  )
+}
+
+export default App
